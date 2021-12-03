95e90a51c4baf7b7b49906bf785e8b475974adc5
Carl Thompson <carl.thompson@opencastsoftware.com>
2021-12-03 16:30:50 +0000

    Day two challenges (and entire restructuring of files for some reason)

--- src/main/scala/Day_2/Dive.scala
+++ src/main/scala/Day_2/Dive.scala
@@ -1,13 +1,59 @@
 package Day_2
 
+import Day_2.Directions.rawDirections
+
+
 object Dive {
 
-  def forward ={
+  val directions = rawDirections.split("\n").toList
+
+  class diver(var horiz: Int = 0, var depth: Int = 0, var aim: Int = 0)
+
+  def goForward(elf: diver, distance: Int) = {
+    elf.horiz += distance
+    elf.depth += elf.aim * distance
+  }
+
+  def goDown(elf: diver, distance: Int) = {
+    //elf.depth += distance
+    elf.aim += distance
+  }
+
+  def goUp(elf: diver, distance: Int) = {
+    //elf.depth -= distance
+    elf.aim -= distance
+  }
+
+  def dives(elf: diver, direction: String, distance: Int) = direction match {
+    case "forward" => goForward(elf, distance)
+    case "down" => goDown(elf, distance)
+    case "up" => goUp(elf, distance)
+  }
+
+  def parseInstruction(listOfInstructions: String) = {
+    val direction: Array[String] = listOfInstructions.split(" ")
+    Array(direction(0), direction(1).toInt)
+  }
+
+  def coordinatesAfterFullDive(elf: diver, input: List[String]): Array[Int] = {
+    val instructions = input
+    for (instruction <- instructions) {
+      val parsedInstruction = parseInstruction(instruction)
+      val direction = parsedInstruction(0).toString
+      val distance = parsedInstruction(1).toString.toInt
+      dives(elf, direction, distance)
+    }
+    Array(elf.horiz, elf.depth)
+  }
+
+  def multiplyHorizontalPotionByDepth(coords: Array[Int]): Int = {
+    coords(0) * coords(1)
 
   }
 
-
   def main(args: Array[String]): Unit = {
-    
+    val scubaElf = new diver()
+    val coordinates: Array[Int] = coordinatesAfterFullDive(scubaElf, directions)
+    println("The answer is" + multiplyHorizontalPotionByDepth(coordinates))
   }
 }
